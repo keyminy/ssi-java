9ad213eeab0c0293839555ab8f98ef963418765e
keyminy <imanaifs@gmail.com>
2023-10-19 15:39:05 +0900

    1:N 채팅

--- src/d4/SimpleServer.java
+++ src/d4/SimpleServer.java
@@ -25,21 +25,18 @@ public class SimpleServer {
 	}
 	private void serverReady() {
 		try {
-			//client가 들어오는지 확인, 들어오면 socket생성
-			Socket s = ss.accept(); 
-			//클라이언트가 들어오면, 보낸 정보(메시지) 추출 -> IO가 필요하다
-			
-			//socket에서 읽어온 데이터를 읽을 수 있음 getInputStream();
-			InputStream is =  s.getInputStream();
-			BufferedReader br = new BufferedReader(new InputStreamReader(is));
-			
-			String data = br.readLine();
-			while(data != null) {
-				System.out.println("Receive : " + data);
-				data = br.readLine();
+			while(true) {
+				//client가 들어오는지 확인, 들어오면 socket생성
+				Socket s = ss.accept(); 
+
+				//클라이언트가 들어오면, Thread생성
+				ServerThread st = new ServerThread(s);
+				Thread thread = new Thread(st);
+				thread.start();
 			}
+			
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 	}
 	
@@ -47,6 +44,36 @@ public class SimpleServer {
 	public static void main(String[] args) {
 		//port open
 		SimpleServer ss = new SimpleServer(9999);
+		
+	}
+	
+	static class ServerThread implements Runnable{
+
+		private Socket socket;
+		
+		public ServerThread(Socket socket) {
+			super();
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				//클라이언트가 들어오면, 보낸 정보(메시지) 추출 -> IO가 필요하다
+				//socket에서 읽어온 데이터를 읽을 수 있음 getInputStream();
+				InputStream is =  socket.getInputStream();
+				BufferedReader br = new BufferedReader(new InputStreamReader(is));
+				
+				String data = br.readLine();
+				while(data != null) {
+					System.out.println("Receive : " + data);
+					data = br.readLine();
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+		}
 		
 	}
 
