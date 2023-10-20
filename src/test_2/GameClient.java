a8ca0ceb6e2a2dbdb4b380b2f4fee542ebe2391a
keyminy <imanaifs@gmail.com>
2023-10-20 20:45:51 +0900

    단방향, 채팅방 입장구현

--- src/test_2/GameClient.java
+++ src/test_2/GameClient.java
@@ -1,10 +1,51 @@
 package test_2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class GameClient {
+	private Socket socket;
+	private BufferedWriter bw;
+	private BufferedReader br;//키보드 연결을 위해
+	
+	
+	public GameClient(String ip, int port) {
+		try {
+			try {
+				socket = new Socket(ip,port);
+				System.out.println("서버 연결 성공");
+				//입력 버퍼 : 닉네임 설정(키보드 입력)
+				br = new BufferedReader(new InputStreamReader(System.in));
+
+				//쓰기버퍼 : 클라 -> 서버
+				OutputStream os = socket.getOutputStream();
+				//PrintWriter는 Auto flush기능이 있음 2번째 매개변수에 true
+				PrintWriter pw = new PrintWriter(os,true);
+				/* 키보드 입력과, 쓰는부분을 while문으로..*/
+				while(true) {
+					System.out.println("사용할 별칭을 입력하세요 : ");
+					String nickName = br.readLine();
+					pw.println(nickName);					
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+//			소켓이 생성되면(서버랑 연결) ClientThread객체 생성
+			ClientThread ct = new ClientThread(socket);
+			Thread thread = new Thread(ct);
+			thread.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 	public static void main(String[] args) {
-
-
+		new GameClient("localhost",9999);
 	}
 
 }
--- src/test_2/GameServer.java
+++ src/test_2/GameServer.java
@@ -1,8 +1,63 @@
 package test_2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameServer {
-	public static void main(String [] args) {
-		
+	
+	private ServerSocket serverSocket; // 클라이언트 연결을 받는 서버 소켓
+	private Socket socket; // 서버 소켓으로부터 생성된, 실제 통신을 하는 소켓
+	private List<Socket> socketList = new ArrayList<>();
+	private BufferedReader br;
+	
+	
+	public GameServer() {
+		this(9999); // 9999포트로 시작
+	}
+
+	public GameServer(int port) {
+		try {
+			// ServerSocket 생성
+			System.out.println("Start Server...");
+			serverSocket = new ServerSocket(port);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		serverReady();
+	}
+
+
+
+
+	private void serverReady() {
+		try {
+			// client가 들어오는지 확인, 들어오면 socket생성
+			socket = serverSocket.accept();
+			socketList.add(socket);
+			
+			//입력 버퍼 생성(클라이언트 입장하였습니다)
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			while (true) {
+				String nickName = br.readLine();
+				System.out.println(nickName+"님이 입장했습니다.");
+				
+			}
+//				GameThread gt = new GameThread(socket,socketList);
+//				Thread thread = new Thread(gt);
+//				thread.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// port open
+		GameServer ss = new GameServer(9999);
 
 	}
+
 }
